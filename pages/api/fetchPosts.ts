7c7174db499c5c04b5203d035ef1945787b9078b
Jeethula <jeethula10@gmail.com>
2024-09-09 15:37:02 +0530

    change in prisma schema to []tags

--- pages/api/fetchPosts.ts
+++ pages/api/fetchPosts.ts
@@ -2,12 +2,18 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from "@/utils/prisma";
 
 export default async function GET(req:NextApiRequest,res:NextApiResponse){
+    
+    try{
     const posts = await prisma.post.findMany({
         orderBy: {
             createdAt: "desc",
         },
     });
     return res.status(200).json(posts);
+}catch(e){
+    console.log(e)
+    return res.status(500).json({error:"Internal Server Error"})
+}
 }
 
 
