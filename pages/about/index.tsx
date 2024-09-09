310802186d62763c376e0ec60ffad2d41f05cacb
Jeethula <jeethula10@gmail.com>
2024-09-09 16:12:39 +0530

    change in about letters

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -3,7 +3,7 @@ import Layout from "../layout";
 function index() {
     return (
         <Layout>
-         <div className="text-3xl font-mono  mt-3">
+         <div className="text-3xl font-mono  mt-5 ml-6 leading-9">
             <p>This is a simple blogging platform where you can post anything without revealing your identity</p>
          </div>
         </Layout>
